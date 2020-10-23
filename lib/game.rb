7ef771fde4138861b27077e9a9e463d7b0e289c1
mke2111 <codejose5@gmail.com>
2020-10-23 03:14:57 +0300

    made required changes

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,7 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/AbcSize
+
 class Game
   attr_accessor :game_on
   attr_reader :board, :winner, :draw
@@ -19,18 +23,23 @@ class Game
   end
 
   def winner?
-    if board[0] == board[1] && board[1] == board[2] || board[3] == board[4] && board[4] == board[5]
-      @winner = true
-    elsif board[6] == board[7] && board[7] == board[8] || board[0] == board[4] && board[4] == board[8]
-      @winner = true
-    elsif board[2] == board[4] && board[4] == board[6] || board[0] == board[3] && board[3] == board[6]
-      @winner = true
-    elsif board[1] == board[4] && board[4] == board[7] || board[2] == board[5] && board[5] == board[8]
-      @winner = true
-    end
+    @win1 = board[0] == board[1] && board[1] == board[2]
+    @win2 = board[3] == board[4] && board[4] == board[5]
+    @win3 = board[6] == board[7] && board[7] == board[8]
+    @win4 = board[0] == board[4] && board[4] == board[8]
+    @win5 = board[2] == board[4] && board[4] == board[6]
+    @win6 = board[0] == board[3] && board[3] == board[6]
+    @win7 = board[1] == board[4] && board[4] == board[7]
+    @win8 = board[2] == board[5] && board[5] == board[8]
+
+    @winner = true if @win1 || @win2 || @win3 || @win4 || @win5 || @win6 || @win7 || @win8
   end
 
   def draw?
-    @draw = true if board.all? { |space| space == 'X' || space == 'O' }
+    @draw = true if board.all? { |space| %w[X O].include? space }
   end
 end
+
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/AbcSize
